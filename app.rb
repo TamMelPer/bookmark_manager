30a775560f748df7af2738337713b1948d95d4f3
Melissa <mystic101@gmail.com>
2020-11-16 17:16:39 +0000

    Viewing Bookmarks

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
   end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'/bookmarks/index'
+  end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
