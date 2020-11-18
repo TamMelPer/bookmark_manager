551b041f411f2c92294b9e80e103cf4a34841ec8
Melissa <mystic101@gmail.com>
2020-11-18 17:59:36 +0000

    STEP11 :(

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -6,7 +6,8 @@ feature 'Adding a new bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/add')
     fill_in('url', with: 'http://mel.com')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
-    expect(page).to have_content 'http://mel.com'
+    expect(page).to have_link('Test Bookmark', href: 'http://mel.com')
   end
 end
